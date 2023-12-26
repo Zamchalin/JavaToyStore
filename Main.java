afe4fdb1e5ec914b2a523e891b2a6eaaabb8594b
Michael <zamchalin@mail.ru>
2023-12-26 19:09:23 +0300

    Added Task

--- Main.java
+++ Main.java
@@ -1,2 +1,17 @@
+import Controller.Controller;
+import Interface.IContainer;
+import Interface.ICustomToy;
+import Model.Container;
+import Model.CustomToy;
+import Model.Toy;
+
+import java.util.*;
+
+
 public class Main {
+    public static void main(String[] args) {
+        Controller controller = new Controller();
+        controller.getView(10);
+
+    }
 }
